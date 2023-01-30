e86829ec4b46e9aca8e575232d729eebdeb7f77b
RitaCavalheiro <44837728+RitaCavalheiro@users.noreply.github.com>
2023-01-30 16:05:11 +0000

    created function for user to select types of characters in their passwords

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let firstPasswordEd = document.getElementById("one")
-let secondPasswordEd = document.getElementById("two")
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const lettersNumbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const lettersSymbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let firstPasswordEd = document.getElementById("one");
+let secondPasswordEd = document.getElementById("two");
 
 async function randomPassword() {
-let passwordLength = document.getElementById("passwordLength").value;
-
- for (let i = 0; i < characters.length; i++) {
-        let password1 = '';
-        let password2 = '';
-
-        if (passwordLength == null || passwordLength <= 0){
+    let passwordLength = document.getElementById("passwordLength").value;
+    let password1 = '';
+    let password2 = '';
+    let includeSymbols = document.getElementById("include-symbols").checked;
+    let includeNumbers = document.getElementById("include-numbers").checked;
+    
+    if (passwordLength == null || passwordLength <= 0){
         alert("Please enter a number bigger than 0 to set the length of your password")
-        }
-
-        let randomNumber = Math.floor(Math.random() * characters.length);
-        password1 += characters[randomNumber];
-        firstPasswordEd.textContent = password1;
-        
-        randomNumber = Math.floor(Math.random() * characters.length);
-        password2 += characters[randomNumber];
-        secondPasswordEd.textContent = password2;
     }
     
+    let charSet;
+    if (includeSymbols && includeNumbers) {
+        console.log(charSet = characters);
+    } else if (includeSymbols) {
+        console.log(charSet = lettersSymbols);
+    } else if (includeNumbers) {
+        console.log(charSet = lettersNumbers);
+    } else {
+        console.log(charSet = letters);
+    }
+
+    for (let i = 0; i < passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * charSet.length);
+        password1 += charSet[randomNumber];
+        firstPasswordEd.textContent = password1;
+        
+        randomNumber = Math.floor(Math.random() * charSet.length);
+        password2 += charSet[randomNumber];
+        secondPasswordEd.textContent = password2;
+    } 
 }
